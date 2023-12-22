f07734666c90a2e0ea42ce8b0ba776ea685f5943
AshutoshDas <ashutosh.rkl.2001@gmail.com>
2023-12-23 01:15:13 +0530

    test commit -- changed IDE

--- src/main/java/com/socials/repository/UserRepository.java
+++ src/main/java/com/socials/repository/UserRepository.java
@@ -10,7 +10,7 @@ import com.socials.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
-	public User findByEmail(String emial);
+	public User findByEmail(String email);
 	
 	@Query("select u from User u where u.firstName LIKE %:query% OR u.lastName LIKE %:query% OR u.email LIKE %:query%")
 	public List<User> searchUser(@Param("query") String query);
