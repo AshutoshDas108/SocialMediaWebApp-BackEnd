8b89bd1b2440db6ed2d36992bb751c09677aecab
AshutoshDas <ashutosh.rkl.2001@gmail.com>
2023-12-23 19:59:31 +0530

    cors configuration added

--- src/main/java/com/socials/config/Appconfig.java
+++ src/main/java/com/socials/config/Appconfig.java
@@ -1,5 +1,6 @@
 package com.socials.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +29,9 @@ public class Appconfig {
 				.requestMatchers("/api/**").authenticated()
 				.anyRequest().permitAll())
 		        .addFilterBefore(new jwtValidator(), BasicAuthenticationFilter.class)
-		        .csrf(csrf -> csrf.disable());
+		        .csrf(csrf -> csrf.disable())
+				.cors(cors->cors.configurationSource(corsConfigurationSource()));
+
 		
 		return http.build();
 	}
@@ -32,6 +40,35 @@ public class Appconfig {
 	@Bean
 	 PasswordEncoder passwordEncoder() {
 		 return new BCryptPasswordEncoder();
+	 }
+
+	 //CORS configuration added
+	 private CorsConfigurationSource corsConfigurationSource(){
+		return  new CorsConfigurationSource() {
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration cfg = new CorsConfiguration();
+				//specify the urls of our front end
+				cfg.setAllowedOrigins(Arrays.asList(
+						"http://localhost:3000",
+						"http://localhost:4000",
+						"http://localhost:4200"
+						//"https://deployed-app link"
+				));
+				//All type of methods are allowed
+				cfg.setAllowedMethods(Collections.singletonList("*"));
+				cfg.setAllowCredentials(true);
+				//All headers are enabled
+				cfg.setAllowedHeaders(Collections.singletonList("*"));
+				//can be added if needed
+				cfg.setExposedHeaders(Arrays.asList(
+						"Authorization"
+				));
+				cfg.setMaxAge(3600L);
+
+				return cfg;
+			}
+		};
 	 }
 
 }
